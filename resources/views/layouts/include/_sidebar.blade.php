bb829cbeec3dbdc05867a60b00e718104bffb5b0
Yudhi151811513042 <50812587+Yudhi151811513042@users.noreply.github.com>
2021-01-18 21:58:25 +0700

    commit yudhi

--- resources/views/layouts/include/_sidebar.blade.php
+++ resources/views/layouts/include/_sidebar.blade.php
@@ -6,42 +6,33 @@
                         <a href="{{ url('/dashboard') }}"><i class
                             ="menu-icon fa fa-laptop"></i>Dashboard </a>
                     </li>
-                    @if(auth()->user()->role == 'admin')
-                        <li class="menu-title">User</li><!-- /.menu-title -->
-                        <li class="">
-                            <a href="{{ url('/user')}}"> <i class="menu-icon fa fa-list"></i>Lihat User</a>
-                            <li class="menu-item-has-children dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-plus"></i>Add User</a>
-                                <ul class="sub-menu children dropdown-menu">
-                                <li><i class="fa fa-user"></i><a href="{{ url('/user/create')}}">Admin</a></li>
-                                    <!-- <li><i class="fa fa-user"></i><a href="{{ url('/user/create/customer')}}">Customer</a></li>
-                                    <li><i class="fa fa-user"></i><a href="{{ url('/user/create/pedagang')}}">Pedagang</a></li> -->
-                                </ul>
-                            </li>
+                    <li class="menu-title">User</li><!-- /.menu-title -->
+                    <li class="">
+                        <a href="{{ url('/user')}}"> <i class="menu-icon fa fa-list"></i>Lihat User</a>
+                        <a href="{{ url('/user/create')}}"> <i class="menu-icon fa fa-plus"></i>Tambah User</a>
+                    </li>
+                    <li class="menu-title">Tabel Aktor</li><!-- /.menu-title -->
+                        <li class="menu-item-has-children dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-user"></i>Customer</a>
+                            <ul class="sub-menu children dropdown-menu">
+                                <li><i class="fa fa-list"></i><a href="">Lihat Customer</a></li>
+                                <li><i class="fa fa-plus"></i><a href="">Tambah Customer</a></li>
+                            </ul>
                         </li>
-                        <li class="menu-title">Tabel Aktor</li><!-- /.menu-title -->
-                            <li class="menu-item-has-children dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-user"></i>Customer</a>
-                                <ul class="sub-menu children dropdown-menu">
-                                    <li><i class="fa fa-list"></i><a href="">Lihat Customer</a></li>
-                                    <li><i class="fa fa-plus"></i><a href="">Tambah Customer</a></li>
-                                </ul>
-                            </li>
-                            <li class="menu-item-has-children dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-user"></i>Pedagang</a>
-                                <ul class="sub-menu children dropdown-menu">
-                                    <li><i class="fa fa-list"></i><a href="">Lihat Pedagang</a></li>
-                                    <li><i class="fa fa-plus"></i><a href="">Tambah Pedagang</a></li>
-                                </ul>
-                            </li>
-                            <li class="menu-item-has-children dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-user"></i>Petani</a>
-                                <ul class="sub-menu children dropdown-menu">
-                                    <li><i class="fa fa-list"></i><a href="">Lihat Petani</a></li>
-                                    <li><i class="fa fa-plus"></i><a href="">Tambah Petani</a></li>
-                                </ul>
-                            </li>
-                    @endif
+                        <li class="menu-item-has-children dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-user"></i>Pedagang</a>
+                            <ul class="sub-menu children dropdown-menu">
+                                <li><i class="fa fa-list"></i><a href="">Lihat Pedagang</a></li>
+                                <li><i class="fa fa-plus"></i><a href="">Tambah Pedagang</a></li>
+                            </ul>
+                        </li>
+                        <li class="menu-item-has-children dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-user"></i>Petani</a>
+                            <ul class="sub-menu children dropdown-menu">
+                                <li><i class="fa fa-list"></i><a href="">Lihat Petani</a></li>
+                                <li><i class="fa fa-plus"></i><a href="">Tambah Petani</a></li>
+                            </ul>
+                        </li>
                     <li class="menu-title">Fitur</li><!-- /.menu-title -->
                     <li class="menu-icon"><img src="{{ asset('images/hope-market.png') }}" width="50px" alt="Hope Market">Hope Market</li><!-- /.menu-title -->
                         <li class="menu-item-has-children dropdown">
@@ -55,6 +46,7 @@
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-table"></i>Kategori Product</a>
                             <ul class="sub-menu children dropdown-menu">
                                 <li><i class="fa fa-list"></i><a href="{{url('/category')}}">Lihat Kategori</a></li>
+                                <li><i class="fa fa-plus"></i><a href="{{url('/category/create')}}">Tambah Kategori</a></li>
                             </ul>
                         </li>
                         <li class="menu-item-has-children dropdown">
@@ -74,9 +66,9 @@
                         </li>
                         <li class="menu-icon"><img src="{{ asset('images/edhope.png') }}" width="50px" alt="Hope Invest">EdHope</li><!-- /.menu-title -->
                         <li class="menu-item-has-children dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-table"></i>Video Edukasi</a>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-table"></i>Materi Video</a>
                             <ul class="sub-menu children dropdown-menu">
-                                <li><i class="fa fa-list"></i><a href="">Lihat Video</a></li>
+                                <li><i class="fa fa-list"></i><a href="{{ url('/edhope')}}">Lihat Video</a></li>
                                 <li><i class="fa fa-plus"></i><a href="">Tambah Video</a></li>
                             </ul>
                         </li>
